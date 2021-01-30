0ace6c444e086abc17a7e931c4bc3b66111205cf
Jose <benavides@estudiantec.cr>
2021-01-29 21:13:01 -0600

    Seeder users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 class UserSeeder extends Seeder
 {
     /**
@@ -17,12 +18,14 @@ class UserSeeder extends Seeder
             'name' => "Jose",
             'email' => 'benavidesvarela@gmail.com',
             'password' => Hash::make('tester123'),
+            'identification' =>'12312300',
             'admin'=> true,
         ]);
       DB::table('users')->insert([
               'name' => "Novlic",
               'email' => 'tecnologianovlic@gmail.com',
               'password' => Hash::make('tester123'),
+              'identification' =>'12312307',
               'admin'=> true,
           ]);
     }
